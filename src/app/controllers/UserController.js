b1a9628bcc91d824f929489be95cabf86403e767
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-08-21 11:08:46 -0300

    final

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,8 +14,9 @@ class UserController {
           .min(6),
       });
 
-      if (!(await schema.isValid(req.body)))
-        return res.status(400).json({ error: 'Validation fails' });
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation failed' });
+      }
 
       const userExists = await User.findOne({
         where: { email: req.body.email },
@@ -25,6 +26,55 @@ class UserController {
         return res.status(400).json({ error: 'Email already exists.' });
 
       const { id, name, email } = await User.create(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+      });
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+        email: Yup.string().email(),
+        oldPassword: Yup.string().min(6),
+        password: Yup.string()
+          .min(6)
+          .when('oldPassword', (oldPassword, field) =>
+            oldPassword ? field.required() : field
+          ),
+        confirmPassword: Yup.string().when('password', (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field
+        ),
+      });
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation failed' });
+      }
+
+      const { email, oldPassword } = req.body;
+
+      const user = await User.findByPk(req.userId);
+      console.log(req.body, req.userId);
+
+      if (email !== user.email) {
+        const userExists = await User.findOne({
+          where: { email },
+        });
+
+        if (userExists)
+          return res.status(400).json({ error: 'Email already exists.' });
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword)))
+        return res.status(401).json({ error: 'Password does not match.' });
+
+      const { id, name } = await User.create(req.body);
 
       return res.json({
         id,
